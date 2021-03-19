741bbffd981a1a2979196e57361d53dd99a5bde1
Iliya-Yadegari <naruto3@email.com>
2021-03-19 20:40:03 +0000

    Update RPS python game.py

--- RPS python game.py
+++ RPS python game.py
@@ -14,7 +14,7 @@ def Prs():
     
     while player == False:
     
-        player = player_ent.get()    
+        player = r.get()  
 
         if player == computer:
             txt = "Tie!"
@@ -36,7 +36,7 @@ def Prs():
         else:
             txt = "That's not a valid play. Check your spelling!"
     
-        res_label = Label(window,text = txt).grid(row = 3, column = 0, padx = 10, pady = 10)
+        res_label = Label(window,text = txt).grid(row = 5, column = 0, padx = 10, pady = 10)
     
     
     
@@ -44,12 +44,15 @@ def Prs():
         
 main_frm = LabelFrame(window)
 
-player_lbl = Label(main_frm,text = 'Rock, Paper, Scissors?').grid(row = 0, column = 0, padx = 10, pady = 10)
-player_ent = Entry(main_frm)
-submit_btn = Button(window,text = 'Submit',width = 20, height = 3,command = Prs).grid(row = 1, column = 0, padx = 10, pady = 10)
-quit_btn = Button(window,text = 'Exit',width = 20, height = 3, command = window.destroy).grid(row = 2, column = 0, pady = 10, padx = 20)
+r = StringVar()
+
+Radiobutton(main_frm,text = 'Rock',variable = r,value = 'Rock').grid(row = 0, column = 0, padx = 10, pady = 10)
+Radiobutton(main_frm,text = 'Paper',variable = r,value = 'Paper').grid(row = 1, column = 0, padx = 10, pady = 10)
+Radiobutton(main_frm,text = 'Scissors',variable = r,value = 'Scissors').grid(row = 2, column = 0, padx = 10, pady = 10)
+
+submit_btn = Button(window,text = 'Submit',width = 20, height = 3,command = Prs).grid(row = 3, column = 0, padx = 10, pady = 10)
+quit_btn = Button(window,text = 'Exit',width = 20, height = 3, command = window.destroy).grid(row = 4, column = 0, pady = 10, padx = 20)
 
 main_frm.grid(row = 0, column = 0, padx = 10, pady = 10)
-player_ent.grid(row = 0, column = 1, padx = 10, pady = 10)
 
 window.mainloop()
